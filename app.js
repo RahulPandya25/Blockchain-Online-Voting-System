e3ef1fc34837459b3f4445a9b3e88626e43e5fcb
Rahul Pandya <rahul25796@gmail.com>
2020-11-22 18:56:52 -0500

    fixed refresh issue

--- app.js
+++ app.js
@@ -14,28 +14,7 @@ const ANGULAR_BUNDLE = "dist/Blockchain-Online-Voting-System";
 
 //make angular dist folder available for public
 app.use(express.static(path.join(__dirname, ANGULAR_BUNDLE)));
-
-function onConnection(socket) {
-  console.log("new user connected");
-  // socket.on("paint", (data) => socket.broadcast.emit("paint", data));
-  // socket.on("mark", (data) => socket.broadcast.emit("mark", data));
-  // socket.on("broadcast", broadcast);
-  // function broadcast(data) {
-  //   if (data.type === "NEW CONNECTION") {
-  //     if (!userService.getUserList().includes(data.user)) {
-  //       userService.addUser(data.user);
-  //       socket.broadcast.emit("new connection", data);
-  //       socket.emit("okay login", userService.getUserList());
-  //     } else socket.emit("username exists");
-  //   } else if (data.type === "USER DISCONNECTED") {
-  //     //  remove user from user list
-  //     userService.removeUser(data.user);
-  //     // broadcast others
-  //     socket.broadcast.emit("user disconnected", data);
-  //   }
-  // }
-}
-
-io.on("connection", onConnection);
+// all the requests should be redirected to angular app
+app.use("*", express.static(path.join(__dirname, ANGULAR_BUNDLE)));
 
 module.exports = app;
