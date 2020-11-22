d67cff98039c5d881cc1eeb74aff4f7fca03953c
Rahul Pandya <rahul25796@gmail.com>
2020-11-22 04:10:42 -0500

    adds voting area in voting screen

--- src/components/voting/voting.component.ts
+++ src/components/voting/voting.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-voting',
-  templateUrl: './voting.component.html',
-  styleUrls: ['./voting.component.scss']
+  selector: "app-voting",
+  templateUrl: "./voting.component.html",
+  styleUrls: ["./voting.component.scss"],
 })
 export class VotingComponent implements OnInit {
+  reason;
+  nominations;
+  choice;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitVote(id) {
+    if (id != "" && this.choice != undefined) {
+      console.log(this.choice);
+      console.log(id);
+    }
   }
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.reason = params["reason"];
+      this.nominations = params["nominations"];
+    });
+  }
 }
